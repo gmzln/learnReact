b3d2dc858ec8ba8ff68bf05056e9fa122a84e0fa
gmzln <gamze_ya@outlook.de>
2025-02-13 09:43:15 +0100

    new folder structure && add handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from "./data";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+  function handleSelect() {
+    console.log("handleSelect");
+  }
+
   return (
     <div>
       <Header />
@@ -21,6 +26,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
