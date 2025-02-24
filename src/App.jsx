85979778bc0a641fb54c15c933b82d71c06e2091
gmzln <gamze_ya@outlook.de>
2025-02-24 13:58:50 +0100

    finish lecture 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,16 @@
-import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept";
-import TabButton from "./components/TabButton";
+import CoreConcepts from "./components/CoreConcept.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  let tabContent = "please click a button";
-  function handleSelect(selectedButton) {
-    //selected button => components, jsx, props, state
-    tabContent = selectedButton;
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            {/* SPREAD all the values of
-            the first object in array instead of the other writing style */}
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
